863f44986cc35c1b4250f43b3b1a72fcff96bfea
Vincenzo Scalcione <54317749+vscalcione@users.noreply.github.com>
2020-05-15 15:07:03 +0200

    Initial commit

---
+++ hello-world.rs
@@ -0,0 +1,10 @@
+fn main(){
+	hello_by_user();
+}
+
+fn hello_by_user(){
+	let mut name = String::new();
+	print!("What's your name? ");
+	std::io::stdin().read_line(&mut name).unwrap();
+	println!("Hello {}", name);
+}
